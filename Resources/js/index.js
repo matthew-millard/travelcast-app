bd69aaa1c8b1bcdd354f480e5de76c4e76a488d8
Matt Millard <mattmillard@Matthews-iMac.JOOWIN>
2023-04-08 12:20:53 -0400

    Feat: add search history accordion

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -1,13 +1,16 @@
 var searchForm = document.querySelector('[data-js="search-form"]')
 var searchInput = searchForm.querySelector('[data-js="city-search"]')
 var historyListEl = searchForm.querySelector('[data-js="history-tab"]')
+var toggleAccordion = searchForm.querySelector('[data-js="search-history-title"]')
 var openWeatherKey = '1ed4313db2c7eabb04ea9a9f7ac7e55e'
+var listOfCities = []
 
 // Get the user's search input
 var searchCityHandler = function (event) {
 	event.preventDefault()
 
 	var cityName = searchInput.value.trim()
+	searchInput.value = ''
 
 	if (!cityName) {
 		return
@@ -31,6 +34,8 @@ var getCityCoordinates = function (cityName) {
 				}
 
 				saveCityToStorage(cityName, cityCoordinates)
+				createHistoryTab(cityName)
+				getCityNamesFromStorage()
 			})
 		} else {
 			console.log(response.statusText)
@@ -43,6 +48,28 @@ var saveCityToStorage = function (cityName, cityCoordinates) {
 	localStorage.setItem(cityName, JSON.stringify(cityCoordinates))
 }
 
-// Create a history tab
+// Get coordinates from local storage
+function getCoordinatesFromStorage() {}
 
+// Get list of city names from local storage
+function getCityNamesFromStorage() {}
+
+var createHistoryTab = function (cityName) {
+	var searchHistory = document.createElement('a')
+	searchHistory.innerText = cityName
+	searchHistory.classList.add('search-history')
+	historyListEl.appendChild(searchHistory)
+}
+
+// Search for a city event handler
 searchForm.addEventListener('submit', searchCityHandler)
+
+// Search history accordion
+toggleAccordion.addEventListener('click', () => {
+	var historyTab = searchForm.querySelector('[data-js="history-tab"]')
+	var plusIndicator = searchForm.querySelector('.fa-plus')
+	var minusIndicator = searchForm.querySelector('.fa-minus')
+	historyTab.classList.toggle('display-none')
+	plusIndicator.classList.toggle('display-none')
+	minusIndicator.classList.toggle('display-none')
+})
